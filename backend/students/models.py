29be9b7e8e41ded8f319c498e3b80507879753c1
OmkarLatambale <omlatambale2002@gmail.com>
2026-09-18 20:35:19 +0530

    Configure environment variables and set up Student model with admin, serializer, and database migrations

--- backend/students/models.py
+++ backend/students/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    age = models.PositiveIntegerField()
+    phone = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
